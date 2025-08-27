967a2ccde1ce16a8ef84d2e3fcb1246c842dbb4d
ANDERSON <ANDERSON@DESKTOP-2D4C51K>
2025-08-26 21:02:42 -0500

    carga inicial del Parcial

---
+++ src/aritmetica/Aritmetica.java
@@ -0,0 +1,23 @@
+
+package aritmetica;
+import java.util.Scanner;
+
+public class Aritmetica {
+    public static void main(String[] args) {
+        //declarando variables
+        double num1, num2, suma, promedio;
+        Scanner lectura = new Scanner(System.in);
+        //entrada de datos
+            System.out.println("Ingresar número 1:");
+            num1=lectura.nextDouble();
+            System.out.println("Ingresar número 2:");
+            num2=lectura.nextDouble();
+           // procesos de datos
+           suma=num1+num2;
+           promedio =suma/2;
+           //salida de datos
+           System.out.println("La suma es:"+suma);
+           System.out.println("El promedio es:"+promedio);
+    }
+    
+}
